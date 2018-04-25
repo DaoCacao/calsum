3ca2624458ccc9b8526dac09ba7e5308e899fc37
Yan Levinsky <LegionCoreHost@gmail.com>
2018-04-25 19:43:09 +0300

    init

---
+++ app/src/main/java/legion/core/calsum/MainActivity.java
@@ -0,0 +1,13 @@
+package legion.core.calsum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
